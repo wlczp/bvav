494ef08459773cdda8c1c7be2a4d6634936600f2
Administrator <bodesc@163.com>
2019-01-14 13:35:28 +0800

    创建项目

---
+++ src/main/java/com/szkj/bva/BvaApplication.java
@@ -0,0 +1,14 @@
+package com.szkj.bva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BvaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BvaApplication.class, args);
+    }
+
+}
+
